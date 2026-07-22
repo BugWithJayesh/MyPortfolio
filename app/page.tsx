a535dc655fc39051157769f57cf2c8fc66ee794e
BugWithJayesh <exe.jayesh@gmail.com>
2026-07-22 20:40:44 +0530

    Add scroll-driven portfolio background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import { Portfolio } from "./components/Portfolio";
+import ScrollSequence from "./components/ScrollSequence";
 
 export default function Home() {
-  return <Portfolio />;
+  return (
+    <ScrollSequence frameCount={300} imageFolder="/scroll-sequence">
+      <Portfolio />
+    </ScrollSequence>
+  );
 }
